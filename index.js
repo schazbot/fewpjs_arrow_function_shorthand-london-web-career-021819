87928607ae37dc91666ebf1d25a20bf1bf0f85de
schazbot <charlotte.pearce1984@gmail.com>
2019-05-27 13:44:27 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1) has a functional expression called divide
+//     2) divide divides 2000 by 100
+//     3) has an arrow function called square
+//     4) square arrow function multiplies a number times itself
+//     5) has an arrow function called add
+//     6) add arrow function takes two parameters and adds them together
+
+function divide() {
+  return 2000 / 100;
+}
+
+ let square = num => {
+ return  num * num;
+};
+
+let add = (x, y) => x + y
